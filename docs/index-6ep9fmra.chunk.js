c72bdcf1090ea89a2376a20363f1a54878c192ed
rynn-kanra <blackhaze879@gmail.com>
2025-09-21 20:51:15 +0700

    * update build

--- docs/index-6ep9fmra.chunk.js
+++
@@ -1,2 +0,0 @@
-import{C as o}from"./index-1r3yxfh0.chunk.js";function r(t){let e;return{get value(){if(e===void 0)e=t();return e}}}var n=r(()=>o(new Worker(new URL("./workers/CanvasWorker.js",import.meta.url),{type:"module"}))),a=r(()=>o(new Worker(new URL("./workers/GutenyeWorker.js",import.meta.url),{type:"module"}))),u={get canvas(){return n.value},get gutenye(){return a.value}},y=u;
-export{y as x};
